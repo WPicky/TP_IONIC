6bd5609ba6f9fab2e42e1c0babb2fa65199d50bd
Jade Llanes <jadellanes97@gmail.com>
2017-11-28 09:26:03 +0100

    video3

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
-//import { VideoPlayer } from '@ionic-native/video-player';
+import { VideoPlayer } from '@ionic-native/video-player';
 import { ToastController } from 'ionic-angular';
 
 @Component({
@@ -12,12 +12,12 @@ export class VideoPage {
   fullPath: string;
 
   constructor(private mediaCapture: MediaCapture,
-             // private videoPlayer: VideoPlayer,
+              private videoPlayer: VideoPlayer,
               public toastCtrl: ToastController) {
   }
 
   takeVideo() {
-    const options: CaptureVideoOptions = { limit: 3 };
+    const options: CaptureVideoOptions = {limit: 3};
     this.mediaCapture.captureVideo(options)
       .then(
         (data: MediaFile[]) => {
@@ -27,10 +27,11 @@ export class VideoPage {
       );
   }
 
- /** playVideo() {
+  playVideo() {
     this.videoPlayer.play(this.fullPath).then(() => {
       console.log('video completed');
     }).catch(err => {
       console.log(err);
-    });*/
+    });
   }
+}
