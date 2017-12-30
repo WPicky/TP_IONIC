128f1198d4a169278be68ae99868fbfdde338173
JadeL <jadellanes97@gmail.com>
2017-12-30 20:12:58 +0100

    scanner

--- src/pages/q-rscanner/q-rscanner.ts
+++ src/pages/q-rscanner/q-rscanner.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
-/**
- * Generated class for the QRscannerPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
